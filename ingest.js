97a4170d66c8d330490007acd92424354fe37127
Sriram <sriram@petavue.com>
2024-12-20 09:32:44 +0530

    added timezone and transactions for drop and create table.
    The entire ingest script is now idempotent and atomic

--- ingest.js
+++ ingest.js
@@ -1,21 +1,23 @@
-import "dotenv/config";
+import 'dotenv/config';
 
-import fs from "fs";
-import csv from "csv-parser";
+import fs from 'fs';
+import csv from 'csv-parser';
 
-import sql from "./db.js";
-import { argv0 } from "process";
+import sql from './db.js';
 
 const SECONDS_IN_AN_HOUR = 3600;
 const SECONDS_IN_A_MINUTE = 60;
 
 const insertBatch = async (sql, batch) => {
-  return await sql`INSERT INTO network_usage ${sql(batch, "username", "mac_address", "start_time", "usage_time", "upload", "download")}`;
+    await sql`SET timezone TO 'UTC';`;
+    return await sql`INSERT INTO network_usage ${sql(batch, 'username', 'mac_address', 'start_time', 'usage_time', 'upload', 'download')}`;
 };
 
-const dropAndCreateTable = async () => {
-  await sql`DROP TABLE IF EXISTS network_usage;`;
-  return sql`
+const dropAndCreateTable = async (sql) => {
+    await sql`SET timezone TO 'UTC';`;
+
+    await sql`DROP TABLE IF EXISTS network_usage;`;
+    return sql`
     CREATE TABLE network_usage (
         id SERIAL PRIMARY KEY,
         username TEXT NOT NULL,
@@ -24,57 +26,55 @@ const dropAndCreateTable = async () => {
         usage_time INT NOT NULL,
         upload NUMERIC NOT NULL,
         download NUMERIC NOT NULL
-    );
-`;
+    );`;
 };
 
 const timeToSeconds = (input) => {
-  // input format hours:minutes:seconds
-  const [hours, minutes, seconds] = input.split(":");
-  return (
-    parseInt(hours) * SECONDS_IN_AN_HOUR +
-    parseInt(minutes) * SECONDS_IN_A_MINUTE +
-    parseInt(seconds)
-  );
+    // input format hours:minutes:seconds
+    const [hours, minutes, seconds] = input.split(':');
+    return (
+        parseInt(hours) * SECONDS_IN_AN_HOUR +
+        parseInt(minutes) * SECONDS_IN_A_MINUTE +
+        parseInt(seconds)
+    );
 };
 
 !(async () => {
-  const filePath = process.env.IMPORT_FILE_PATH || "data.csv";
+    const filePath = process.env.IMPORT_FILE_PATH || 'data.csv';
 
-  await dropAndCreateTable();
+    const parser = fs.createReadStream(filePath).pipe(csv());
 
-  const parser = fs.createReadStream(filePath).pipe(csv());
+    let batch = [];
+    let processedRows = 0;
+    const batchSize = 500;
+    let totalRowsProcessed = 0;
+    let batchNo = 1;
 
-  let batch = [];
-  let processedRows = 0;
-  const batchSize = 500;
-  let totalRowsProcessed = 0;
-  let batchNo = 1;
+    await sql.begin(async (sql) => {
+        await dropAndCreateTable(sql);
+        for await (const record of parser) {
+            record.start_time = new Date(record.start_time);
+            record.usage_time = timeToSeconds(record.usage_time);
+            record.upload = parseFloat(record.upload);
+            record.download = parseFloat(record.download);
 
-  await sql.begin(async (sql) => {
-    for await (const record of parser) {
-      record.start_time = new Date(record.start_time);
-      record.usage_time = timeToSeconds(record.usage_time);
-      record.upload = parseFloat(record.upload);
-      record.download = parseFloat(record.download);
+            batch.push(record);
+            processedRows++;
+            totalRowsProcessed++;
 
-      batch.push(record);
-      processedRows++;
-      totalRowsProcessed++;
+            if (processedRows === batchSize) {
+                console.log(`Inserting batch ${batchNo}`);
+                await insertBatch(sql, batch);
+                processedRows = 0;
+                batch = [];
+                batchNo++;
+            }
+        }
 
-      if (processedRows === batchSize) {
-        console.log(`Inserting batch ${batchNo}`);
-        await insertBatch(sql, batch);
-        processedRows = 0;
-        batch = [];
-        batchNo++;
-      }
-    }
+        if (processedRows > 0) {
+            await insertBatch(sql, batch);
+        }
+    });
 
-    if (processedRows > 0) {
-      await insertBatch(sql, batch);
-    }
-  });
-
-  process.exit(0);
+    process.exit(0);
 })();
